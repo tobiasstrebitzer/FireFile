3e75b9bb13182482c540921cbc1265b2e77d035e
tobias.strebitzer <tobias.strebitzer@holzweg2.com>
2010-11-12 16:47:59 +0100

    initial commit

---
+++ defaults/preferences/default.js
@@ -0,0 +1,4 @@
+pref("extensions.firefile.sites", "");
+pref("extensions.firefile.enable_notifications", true);
+pref("extensions.firefile.inspector_switch_css", true);
+pref("extensions.firefile.debug", false);
